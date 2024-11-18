cb9136799b1a54ed73be9df9f70faba67f415975
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 14:23:04 +0100

    declared function checkPalindrome and add eventListener on the button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
-console.log(textInput);
-console.log(checkBtn);
-console.log(result);
+
+const checkPalindrome = () => {
+  console.log("Here");
+};
+checkBtn.addEventListener("click", checkPalindrome);
