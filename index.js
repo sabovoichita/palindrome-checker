158f7b27bf22cf8f93d766ece1e89df4bba13674
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 14:21:09 +0100

    declare const variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Welcome!");
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+console.log(textInput);
+console.log(checkBtn);
+console.log(result);
