13220ec338c418ead7eb926795e34bf57d79b65f
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-18 14:40:30 +0100

    declare user input and inputReversed variables

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-const checkPalindrome = () => {
-  console.log("Here");
+let checkPalindrome = () => {
+  const regex = /[\W_]/gi;
+  let userInput = textInput.value.replace(regex, "").toLowerCase();
+  console.log("Here1: ", userInput);
+  let userInputReverse = userInput.split("").reverse().join("").toLowerCase();
+  console.log("Here Reversed2: ", userInputReverse);
 };
 checkBtn.addEventListener("click", checkPalindrome);
